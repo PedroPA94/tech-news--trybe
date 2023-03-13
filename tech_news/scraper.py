3412abb1cf76e5bb93c9ad84f3daef5f3505abed
Pedro Papini <ppapinia@gmail.com>
2023-03-13 15:36:50 -0300

    Implementa função scrape_updates

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -18,7 +19,8 @@ def fetch(url):
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css('.entry-title a::attr(href)').getall()
 
 
 # Requisito 3
