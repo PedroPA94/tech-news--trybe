ca893cec55b3b6aef58d8e973ad9ad17c27ef4bd
Pedro Papini <ppapinia@gmail.com>
2023-03-14 16:36:21 -0300

    Implementa busca por título

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,21 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {
+        "title": {
+            "$regex": title,
+            "$options": "i"
+        }
+    }
+
+    matches = search_news(query)
+
+    return [
+        (news["title"], news["url"])
+        for news in matches
+    ]
 
 
 # Requisito 8
