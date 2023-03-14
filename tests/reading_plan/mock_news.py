527efd7bd4723902a358abd1469124dd9b525a11
Pedro Papini <ppapinia@gmail.com>
2023-03-14 16:16:02 -0300

    Adiciona testes para ReadingPlanService

---
+++ tests/reading_plan/mock_news.py
@@ -0,0 +1,38 @@
+mock_news = [
+    {
+        'url': 'https://blog.betrybe.com/desenvolvimento-web/angular-cli/',
+        'title': 'Angular CLI: como usar essa interface de linha de comando?',
+        'timestamp': '08/03/2023',
+        'writer': 'Cairo Noleto',
+        'reading_time': 5,
+        'summary': 'Super notícia',
+        'category': 'Desenvolvimento Web'
+    },
+    {
+        'url': 'https://blog.betrybe.com/desenvolvimento-web/angular-cli/',
+        'title': 'Teste seu código',
+        'timestamp': '08/03/2023',
+        'writer': 'Cairo Noleto',
+        'reading_time': 5,
+        'summary': 'Super notícia',
+        'category': 'Desenvolvimento Web'
+    },
+    {
+        'url': 'https://blog.betrybe.com/desenvolvimento-web/angular-cli/',
+        'title': 'Como usar Typescript',
+        'timestamp': '08/03/2023',
+        'writer': 'Cairo Noleto',
+        'reading_time': 15,
+        'summary': 'Super notícia',
+        'category': 'Desenvolvimento Web'
+    },
+    {
+        'url': 'https://blog.betrybe.com/desenvolvimento-web/angular-cli/',
+        'title': 'Decaimento do vácuo é inevitável',
+        'timestamp': '08/03/2023',
+        'writer': 'Cairo Noleto',
+        'reading_time': 11,
+        'summary': 'Super notícia',
+        'category': 'Desenvolvimento Web'
+    },
+]
--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,21 @@
+import pytest
 from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from unittest.mock import MagicMock
+from .mock_news import mock_news
 
 
 def test_reading_plan_group_news():
-    pass
+    ReadingPlanService._db_news_proxy = MagicMock(return_value=mock_news)
+
+    with pytest.raises(ValueError, match="maior que zero"):
+        ReadingPlanService.group_news_for_available_time(-10)
+
+    plan = ReadingPlanService.group_news_for_available_time(12)
+
+    assert len(plan["readable"]) == 2
+    assert len(plan["unreadable"]) == 1
+
+    print(plan["readable"])
+
+    assert plan["readable"][0]["unfilled_time"] == 2
+    assert plan["readable"][1]["unfilled_time"] == 1
