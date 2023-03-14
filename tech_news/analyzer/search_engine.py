8a098ab377434016501807f728f18b457d44a38e
Pedro Papini <ppapinia@gmail.com>
2023-03-14 16:48:11 -0300

    Implementa busca por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -20,7 +21,18 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date_ISO = datetime.strptime(date, '%Y-%m-%d').date()
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    date_str = date_ISO.strftime('%d/%m/%Y')
+    matches = search_news({"timestamp": {"$eq": date_str}})
+
+    return [
+        (news["title"], news["url"])
+        for news in matches
+    ]
 
 
 # Requisito 9
